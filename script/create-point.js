34aab1296e76b5f68f5b19a5b0c9178130188ce3
JardelPraxedes <jardelpraxedes111@gmail.com>
2020-11-12 00:15:09 -0300

    test

---
+++ script/create-point.js
@@ -0,0 +1,43 @@
+function populateUFs () {
+    const ufSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => res.json() )
+    .then( states => {
+    
+        for( const state of states) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+
+ populateUFs () 
+ 
+ function getCities(event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectState].text
+
+    const indexOfSelectState = event.target.selectedIndex
+
+    const url =`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then(res => res.json() )
+    .then( cities => {
+    
+        for( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+
+        citySelect.disabled = false
+        }
+    }
+
+
+
+document
+    .querySelector("select[name=uf]") 
+    .addEventListener("change", getCities)
